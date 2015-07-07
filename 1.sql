698043637e4a2d1d65ffa4e1314c4288bb7aa117
alexey.gimadiev <alexey.gimadiev@gmail.com>
2015-07-07 12:22:30 +0300

    fine inner join

--- 1.sql
+++ 1.sql
@@ -1,2 +1,3 @@
 select * from ds_orders
-inner join ds_orders_items on ds_orders.masterfid = ds_orders_items.masterfid
+inner join ds_orders_items on ds_orders.masterfid = ds_orders_items.masterfid and ds_orders.orid = ds_orders_items.orid
+
