07a21df6ac69d091cafb66855bb1cf395745e394
alexey.gimadiev <alexey.gimadiev@gmail.com>
2015-07-07 12:25:51 +0300

    ds_items

--- 1.sql
+++ 1.sql
@@ -1,3 +1,4 @@
 select * from ds_orders
 inner join ds_orders_items on ds_orders.masterfid = ds_orders_items.masterfid and ds_orders.orid = ds_orders_items.orid
+inner join ds_items on ds_items.iid = ds_orders_items.iid
 
